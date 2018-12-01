0b777d22276fe0ad9663538ac6ca908bc6f15cb7
Kitty <katherine.overman.2019@student.lhschools.org>
2018-12-01 18:13:42 -0500

    Re-arrange, rename

--- src/Manufacturing.java
+++ src/Manufacturing.java
@@ -1,23 +1,34 @@
+import java.util.Scanner;
 
-    public static class double calcCost(int numOfSuppliesNeeded, Scanner scanner){
-
-        for(int i =1; i <= numOfSuppliesNeeded; i++){
-        print.ln(“How many did you buy of supply item ” + i);
-        numOfItems = scanner.nextInt();
-        print.ln (“How much per item?”);
-        cost = scanner.nextDouble();
-        total = total + (numOfItems*cost);
-        }
-        return total;
-        }
-
-
-        import java.util.Scanner;
-        main {
-        doubleTotalCost;
+public class Manufacturing {
+    public static void main(String[] args) {
+        double totalCost, subtotal, profit;
+        int totalSales, days, numOfItems;
         Scanner keyboard;
-        intItemsNeeded;
-        totalCost = calcCost(numOfSuppliesNeeded; keyboard);
-        totalSales = method2(days, keyboard);
-        profit = profit(totaCost, totalSales);
-        }
+        totalCost=calcCost(numOfItems;keyboard);
+        totalSales=(days,keyboard);
+        profit=profit(totalCost,totalSales);
+
+    }
+}
+public double calcCost(int numOfItems, double itemCost) {
+
+    //declare
+    double totalCost, subtotal;
+    int totalSales, days;
+    Scanner keyboard;
+
+
+    //initializing
+
+    for (int i = 1; i < numOfItems; i++) {
+        System.out.println("How many did you buy of supply item " + i);
+        numOfItems = keyboard.nextInt();
+        System.out.println("How much per item ?");
+        itemCost = keyboard.nextDouble();
+        subtotal = subtotal + (numOfItems * itemCost);
+    }
+    return totalCost;
+}
+
+public double profit(double totalCost, int totalSales);
