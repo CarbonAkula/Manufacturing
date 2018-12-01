e5cf3b10eb7485f304f6db57b9756ffc544444e5
Kitty <katherine.overman.2019@student.lhschools.org>
2018-12-01 16:40:07 -0500

    Coding Start

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,52 +1,23 @@
-public class MathTester {
 
-public static void main(String[] args) {
-    int firstNumber = 10;
-    int secondNumber = 20;
-    String firstWord;
-    String secondWord;
-    int total;
+    public static double calcCost(int numOfSuppliesNeeded, Scanner scanner){
 
-    if(firstNumber>secondNumber) {
-        System.out.println(firstNumber +" is less");
-    }
-    else if(firstNumber<secondNumber) {
-        System.out.println(secondNumber +" is greater");
-    }
-    else{
-        System.out.println("I'm not sure.");
-    }
+        for(int i =1; i <= numOfSuppliesNeeded; i++){
+        print.Ln(“How many did you buy of supply item ” + i);
+        numOfItems = scanner.nextInt();
+        print.Ln (“How much per item?”);
+        cost = scanner.nextDouble();
+        total = total + (numOfItems*cost);
+        }
+        return total;
+        }
 
 
-
-
-
-
-    System.out.println("**********************************************************");
-
-
-
-
-
-    firstWord = "dog";
-    secondWord = "house";
-    firstNumber = 5;
-    secondNumber = 10;
-    total = firstNumber+secondNumber;
-    System.out.println(total);
-
-
-
-    System.out.println("firstNumber + secondNumber is "+ (firstNumber+secondNumber));
-    System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
-    System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
-    System.out.println("firstNumber * secondNumber is "+ (firstNumber*secondNumber));
-    System.out.println("The modulus of the secondNumber by the firstNumber"+
-            " is "+ (secondNumber%firstNumber));
-
-
-
-
-
-}
-}
+        import java.util.Scanner;
+        main {
+        doubleTotalCost;
+        Scanner keyboard;
+        intItemsNeeded
+        totalCost = calcCost(numOfSuppliesNeeded; keyboard);
+        totalSales = method2(days, keyboard);
+        profit = profit(totaCost, totalSales);
+        }
