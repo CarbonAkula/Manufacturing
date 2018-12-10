fecad340a10eadf9c40e44372326e7038a28e95b
Kitty <katherine.overman.2019@student.lhschools.org>
2018-12-10 09:57:38 -0500

    Fixed errors, added return

--- src/Manufacturing.java
+++ src/Manufacturing.java
@@ -1,13 +1,11 @@
-
-
 import java.util.Scanner;
 
 public class Manufacturing {
     public static void main(String[] args) {
-        double totalCost, subtotal, profit;
+        double totalCost, subtotal, profit, itemCost;
         int totalSales, days, numOfItems;
         Scanner keyboard;
-        totalCost=calcCost(numOfItems,keyboard);
+        totalCost=calcCost(numOfItems,itemCost);
         totalSales=(days,keyboard);
         profit=profit(totalCost,totalSales);
 
@@ -22,15 +20,20 @@ public static double calcCost(int numOfItems, double itemCost) {
 
 
     //initializing
+    keyboard = new
+            Scanner(System.in);
 
     for (int i = 1; i < numOfItems; i++) {
         System.out.println("How many did you buy of supply item " + i + "?");
         numOfItems = keyboard.nextInt();
         System.out.println("How much per item?");
         itemCost = keyboard.nextDouble();
-        subtotal = subtotal + (numOfItems * itemCost);
+        subtotal += numOfItems * itemCost;
+        totalCost = subtotal;
     }
     return totalCost;
 }
 
-public static double profit(double totalCost, int totalSales){}
+public static double profit(double totalCost, int totalSales){
+    return profit;
+}}
